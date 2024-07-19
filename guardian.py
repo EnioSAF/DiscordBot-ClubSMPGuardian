fa2a0ed55f8717e03233b9b094ffaa8fd02051e5
EnioSadFlower <98952049+EnioSAF@users.noreply.github.com>
2024-07-19 12:21:47 +0200

    /say command test

--- guardian.py
+++ guardian.py
@@ -1,23 +1,28 @@
 import discord
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN') # Import Bot Token in .env locally
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Connecté en tant que {client.user}')
+    print(f'Connecté en tant que {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(name='say')
+@commands.has_permissions(administrator=True)
+async def say(ctx, *, message: str):
+    await ctx.send(message)
 
-    await message.channel.send(message.content)
+@say.error
+async def say_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Vous n'avez pas la permission d'utiliser cette commande.")
 
-client.run(TOKEN)
+bot.run(TOKEN)
