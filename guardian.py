ad6351d7cec54118d9e0dfa8c16f6fe0230696c1
EnioSadFlower <98952049+EnioSAF@users.noreply.github.com>
2024-07-19 13:21:40 +0200

    Added command discord menu

--- guardian.py
+++ guardian.py
@@ -1,28 +1,34 @@
 import discord
-from dotenv import load_dotenv
-import os
 from discord.ext import commands
+from discord import app_commands
+import os
+from dotenv import load_dotenv
 
+# Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
-TOKEN = os.getenv('DISCORD_BOT_TOKEN') # Import Bot Token in .env locally
+TOKEN = os.getenv('DISCORD_BOT_TOKEN') # BOT TOKE
+GUILD_ID = os.getenv('GUILD_ID') # DISCORD SERVER ID
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Connecté en tant que {bot.user}')
+    try:
+        synced = await bot.tree.sync(guild=discord.Object(id=int(GUILD_ID)))
+        print(f'Synchronisé {len(synced)} commandes')
+    except Exception as e:
+        print(f'Erreur lors de la synchronisation: {e}')
 
-@bot.command(name='say')
-@commands.has_permissions(administrator=True)
-async def say(ctx, *, message: str):
-    await ctx.send(message)
+@bot.tree.command(name='say', description='Répète le message spécifié', guild=discord.Object(id=int(GUILD_ID)))
+@app_commands.checks.has_permissions(administrator=True)
+async def say(interaction: discord.Interaction, message: str):
+    await interaction.response.send_message(message)
 
 @say.error
-async def say_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("Vous n'avez pas la permission d'utiliser cette commande.")
+async def say_error(interaction: discord.Interaction, error):
+    if isinstance(error, app_commands.MissingPermissions):
+        await interaction.response.send_message("Vous n'avez pas la permission d'utiliser cette commande.", ephemeral=True)
 
 bot.run(TOKEN)
